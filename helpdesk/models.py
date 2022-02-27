170400008821887b90ea8d1a0cd1a843e804737e
adrien <adrien.schmitz@gmail.com>
2022-02-27 09:29:36 -0300

    models

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class usuarios(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+
+class solicitante(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+class solicitacoes(models.Model):
+    descricao = models.TextField()
+    usuario = models.ForeignKey(usuarios, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, default='Aberto')
+
+
+class perfis(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+
+class setores(models.Model):
+    nome = models.CharField(max_length=100)
+
+
+class locais(models.Model):
+    nome = models.CharField(max_length=100)
+    setor = models.ForeignKey(setores, on_delete=models.CASCADE)
