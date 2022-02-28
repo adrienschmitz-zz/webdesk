b0fe8d26fcadd4ef93312ab3dc0e7a69afafddab
adrien <adrien.schmitz@gmail.com>
2022-02-28 08:04:06 -0300

    models ajustment

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,59 +1,25 @@
 from django.contrib import admin
-from .models import Usuarios, Solicitantes, Solicitacoes, Perfis, Setores, Locais
-
-
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'email')
-
-    class Meta:
-        verbose_name = 'Usuário'
-        verbose_name_plural = "Usuarios"
+from .models import Solicitantes, Solicitacoes,  Setores, Locais
 
 
 class SolicitanteAdmin(admin.ModelAdmin):
     list_display = ('nome',)
 
-    class Meta:
-        verbose_name = 'Solicitante'
-        verbose_name_plural = "Solicitantes"
+
+class SetorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+
+class LocalAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'setor')
 
 
 class SolicitacaoAdmin(admin.ModelAdmin):
     list_display = ('descricao', 'usuario', 'data_criacao',
                     'data_atualizacao', 'status')
 
-    class Meta:
-        verbose_name = 'Solicitação'
-        verbose_name_plural = "Solicitações"
 
-
-class PerfilAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'descricao')
-
-    class Meta:
-        verbose_name = 'Perfil'
-        verbose_name_plural = "Perfis"
-
-
-class SetorAdmin(admin.ModelAdmin):
-    list_display = ('nome',)
-
-    class Meta:
-        verbose_name = 'Setor'
-        verbose_name_plural = "Setores"
-
-
-class LocalAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'setor')
-
-    class Meta:
-        verbose_name = 'Local'
-        verbose_name_plural = "Locais"
-
-
-admin.site.register(Usuarios, UsuarioAdmin)
 admin.site.register(Solicitantes, SolicitanteAdmin)
-admin.site.register(Solicitacoes, SolicitacaoAdmin)
-admin.site.register(Perfis, PerfilAdmin)
 admin.site.register(Setores, SetorAdmin)
 admin.site.register(Locais, LocalAdmin)
+admin.site.register(Solicitacoes, SolicitacaoAdmin)
--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,32 +1,51 @@
 from django.db import models
-
-
-class Usuarios(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-
-
-class Solicitantes(models.Model):
-    nome = models.CharField(max_length=100)
-
-
-class Solicitacoes(models.Model):
-    descricao = models.TextField()
-    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_atualizacao = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=100, default='Aberto')
-
-
-class Perfis(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
+from django.contrib.auth.models import User
 
 
 class Setores(models.Model):
     nome = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Setor'
+        verbose_name_plural = "Setores"
+
 
 class Locais(models.Model):
     nome = models.CharField(max_length=100)
     setor = models.ForeignKey(Setores, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Local'
+        verbose_name_plural = "Locais"
+
+
+class Solicitantes(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Solicitante'
+        verbose_name_plural = "Solicitantes"
+
+
+class Solicitacoes(models.Model):
+    descricao = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local = models.ForeignKey(Locais, on_delete=models.CASCADE)
+    solicitante = models.ForeignKey(
+        Solicitantes, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, default='Aberto')
+
+    class Meta:
+        verbose_name = 'Solicitação'
+        verbose_name_plural = "Solicitações"
