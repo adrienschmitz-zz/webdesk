032cc1a83e30e356c99709d18be4ff5a455eec12
adrien <adrien.schmitz@gmail.com>
2022-04-17 17:56:18 -0300

    tried to do a dark mode

---
+++ context_processors/context_processors.py
@@ -0,0 +1,13 @@
+from helpdesk.models import Preferences
+
+
+def pref(request):
+    if request.user.is_authenticated:
+        _preferences = Preferences.objects.filter(user=request.user).last()
+
+    else:
+        _preferences = None
+
+    return {
+        'user_pref': _preferences,
+    }
